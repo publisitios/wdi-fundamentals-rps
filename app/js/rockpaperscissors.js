21764a1f1f11d53de362367e5885c26618d39b6b
publisitios <xavier@publisitios.cr>
2015-02-21 14:18:05 -0600

    homework 4 completed

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -41,6 +41,27 @@ function getWinner(playerMove,computerMove) {
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
     /* YOUR CODE HERE */
+
+    
+    /*player wins cases */
+    if(playerMove == "rock" && computerMove == "scissors")
+    {winner="player";}
+    else if(playerMove == "scissors" && computerMove == "paper")
+    {winner="player";}
+    else if(playerMove == "paper" && computerMove == "rock")
+    {winner="player";}
+
+    /*computer wins cases */
+    else if(playerMove == "rock" && computerMove == "paper")
+    {winner="computer";}
+    else if(playerMove == "scissors" && computerMove == "rock")
+    {winner="computer";}
+    else if(playerMove == "paper" && computerMove == "scissors") 
+    {winner="computer";}
+    
+    /*tie cases */
+    else if(playerMove == computerMove)
+    {winner="tie";}
     return winner;
 }
 
